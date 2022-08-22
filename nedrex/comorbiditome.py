2315d80afff1b5e4b424a8373c6ecfc2e77aee4f
james-skelton <james.skelton@newcastle.ac.uk>
2022-08-22 16:35:39 +0100

    Added function accessing API route to get ICD-10 associations

--- nedrex/comorbiditome.py
+++ nedrex/comorbiditome.py
@@ -4,6 +4,7 @@
 
 from typing import Dict as _Dict
 from typing import List as _List
+from typing import Literal
 
 from nedrex import config as _config
 from nedrex._common import check_response as _check_response
@@ -27,6 +28,25 @@ def map_mondo_to_icd10(
     url = f"{_config.url_base}/comorbiditome/mondo_to_icd10"
 
     params = {"mondo": disorders, "only_3char": only_3char, "exclude_3char": exclude_3char}
+    headers = {"x-api-key": _config.api_key}
+
+    response = _http.get(url, params=params, headers=headers)
+    result: _Dict[str, _List[str]] = _check_response(response)
+    return result
+
+
+_EdgeTypes = Literal[
+    "gene_associated_with_disorder",
+    "drug_has_indication",
+    "drug_has_contraindication",
+    "drug_targets_disorder_associated_gene_product",
+]
+
+
+def get_icd10_associations(nodes: _List[str], edge_type: _EdgeTypes) -> _Dict[str, _List[str]]:
+    url = f"{_config.url_base}/comorbiditome/get_icd10_associations"
+
+    params = {"node": nodes, "edge_type": edge_type}
     headers = {"x-api-key": _config.api_key}
 
     response = _http.get(url, params=params, headers=headers)
